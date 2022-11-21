e870392f289300d9c6c8febb4c78651378580c70
shanicesauce <shanicesauceda@gmail.com>
2022-11-21 16:17:07 -0600

    created about file

---
+++ src/components/About/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import coverImage from '../../';
+
+const About = () => {
+  return (
+    <section className="my-5">
+      <h1 id="about">Who am I?</h1>
+    </section>
+  );
+};
+export default About;
