acc222def73da543227d96a6eb594e896ec97094
shanicesauce <shanicesauceda@gmail.com>
2022-11-23 17:33:43 -0600

    tested about component

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, cleanup } from "@testing-library/react";
+
+import About from "..";
+const { asFragment } = render (<About />);
+
+describe("About component", () => {
+  //renders About test
+  //test 1
+  it("renders", () => {
+    render(<About />);
+  });
+  //test 2
+  it('matches snapshot DOM node structure', () => {
+    expect(asFragment()).toMatchSnapshot();
+  })
+});
+
+afterEach(cleanup);
